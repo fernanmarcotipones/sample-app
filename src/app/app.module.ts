c6ddf45ce10c6c151ec1b7c2247d4b06421665b0
Fernan Marco Tipones <ftipones@legalsight.com>
2022-05-14 22:08:25 +0800

    Added login page and custom classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicModule } from './public/public.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PublicModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
